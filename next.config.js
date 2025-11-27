97053ea941e396ad2deb039de4498cdbc0d9388b
Touhidur Rahman <rahmantouhidor@gmail.com>
2025-11-27 12:56:55 +0600

    Remove experimental optimizeCss to fix build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'unsplash.com', 'placehold.co'],
+    domains: ['images.unsplash.com', 'unsplash.com', 'placehold.co', 'i.ytimg.com', 'img.youtube.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
-  },
-  experimental: {
-    optimizeCss: true,
   },
 }
 
