a42ccb06c7d832f7921cc29d4b167c9a71796966
Touhidur Rahman <rahmantouhidor@gmail.com>
2025-12-05 00:34:36 +0600

    Update page.tsx: Remove Stats/Certifications, add VisaProcessing

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,24 @@
 import Hero from '@/components/sections/Hero'
 import About from '@/components/sections/About'
 import Programs from '@/components/sections/Programs'
-import Certifications from '@/components/sections/Certifications'
 import Facilities from '@/components/sections/Facilities'
 import Process from '@/components/sections/Process'
 import Testimonials from '@/components/sections/Testimonials'
-import Stats from '@/components/sections/Stats'
 import FAQ from '@/components/sections/FAQ'
 import Team from '@/components/sections/Team'
 import Videos from '@/components/sections/Videos'
-
+import VisaProcessing from '@/components/sections/VisaProcessing'
 export default function Home() {
   return (
     <main className="relative overflow-hidden">
       <Hero />
-      <Stats />
       <About />
       <Programs />
       <Process />
-      <Certifications />
       <Facilities />
       <Videos />
       <Testimonials />
+      <VisaProcessing />
       <Team />
       <FAQ />
     </main>
